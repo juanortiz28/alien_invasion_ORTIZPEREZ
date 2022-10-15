099892f6efefe8102a551db41ce34f9dea5954c2
juanortiz28 <m254839@usna.edu>
2022-10-15 17:07:07 -0400

    refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,17 +25,24 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.bg_color)
+    def _check_events(self):
+        """"respond to keys and mouse"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            self.screen.fill(self.settings.bg_color)
+    def _update_screen(self):
+        """updates images on screen and flip to the new screen"""
+        self.screen.fill(self.bg_color)
 
-            self.ship.blitme()  #draws ship on screen
+        self.screen.fill(self.settings.bg_color)
 
-            pygame.display.flip()
+        self.ship.blitme()  #draws ship on screen
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
